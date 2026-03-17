25d05c89167712e85e76108a3e6ecd9642a3a8c3
TomasGimenezK <tgimenezkesseru@mail.austral.edu.ar>
2026-03-17 12:50:44 -0300

    Update change.py

--- change.py
+++ change.py
@@ -2,4 +2,14 @@ def change():
     """Lee un gasto y el dinero recibido, calcula el vuelto
     y lo separa en pesos (parte entera) y centavos.
     """
+    print("Ingresar gasto:")
+    precio = input()
+    print(precio)
+    print("Dinero recibido")
+    dinero = input()
+    print(dinero)
+    vuelto = int(dinero) - float(precio)
+    centavos = int(vuelto*100 - int(vuelto)*100)
+    pesos = int(vuelto)
+    print(f"\nVuelto\n\nPesos:\n{pesos}\nCentavos:\n{centavos}")
     pass
