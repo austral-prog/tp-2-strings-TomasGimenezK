2ed4559e9f70b28224b95a6f8d7d4771dee43153
TomasGimenezK <tgimenezkesseru@mail.austral.edu.ar>
2026-03-17 12:52:13 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,4 +2,13 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
+
+    nombre = input("Nombre: ")
+    apellido = input("Apellido: ")
+    print(nombre.lower()+ " " + apellido.lower())
+    print(nombre.title() + " " + apellido.title())
+    print(nombre.upper() + " " + apellido.upper())
+    print("\t" + nombre.lower() + " " + apellido.lower())
     pass
+
+
